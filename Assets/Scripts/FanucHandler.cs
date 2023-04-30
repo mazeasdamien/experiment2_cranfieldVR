e4eba1d0f5340ae0dab865a534966349a3050162
Damien Mazeas <58029218+mazeasdamien@users.noreply.github.com>
2023-04-30 13:50:00 +0100

    30/04/2023

--- Assets/Scripts/FanucHandler.cs
+++ Assets/Scripts/FanucHandler.cs
@@ -1,77 +1,117 @@
-using FRRobot;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
 using UnityEngine;
+using System.Threading.Tasks;
+
 
 namespace Telexistence
 {
     public class FanucHandler : MonoBehaviour
     {
-        public string robotIpAddress = "127.0.0.1";
+        private TcpClient _client;
+        private StreamReader _reader;
+        private StreamWriter _writer;
+
+        private string _serverIP = "127.0.0.1";
+        private int _port = 5000;
+
         public Transform kinect_cursor;
         public Transform worldPosition;
-        FRCRobot _robot;
         public List<Transform> robot = new List<Transform>();
-        private Vector3 tempPos = new();
-        private Vector3 tempRot = new();
 
         void Start()
         {
-            ConnectToRobot();
+            ConnectToServer();
+            ReadDataFromServerAsync();
         }
 
-        void Update()
-        {
-            if (_robot != null && _robot.IsConnected)
-            {
-                GetCurrentJointPositions();
-                RegisterNewPoseIfChanged();
-            }
-        }
-
-        public void ConnectToRobot()
+        private void ConnectToServer()
         {
             try
             {
-                _robot = new();
-                _robot.ConnectEx(robotIpAddress, false, 10, 1);
-                Debug.Log("Connected to robot successfully.");
+                _client = new TcpClient(_serverIP, _port);
+                _reader = new StreamReader(_client.GetStream());
+                _writer = new StreamWriter(_client.GetStream());
+                Debug.Log("Connected to server successfully.");
             }
             catch (Exception e)
             {
-                Debug.LogError("Failed to connect to robot: " + e.Message);
+                Debug.LogError("Failed to connect to server: " + e.Message);
             }
         }
 
-        void GetCurrentJointPositions()
+        private async void ReadDataFromServerAsync()
         {
-            var curPosition = _robot.CurPosition;
-            var groupPositionJoint = curPosition.Group[1, FRECurPositionConstants.frJointDisplayType];
-            var groupPositionWorld = curPosition.Group[1, FRECurPositionConstants.frWorldDisplayType];
-            groupPositionJoint.Refresh();
-            var joint = (FRCJoint)groupPositionJoint.Formats[FRETypeCodeConstants.frJoint];
-            var xyzWpr = (FRCXyzWpr)groupPositionWorld.Formats[FRETypeCodeConstants.frXyzWpr];
+            if (_reader == null) return;
 
-            worldPosition.localPosition = new Vector3(-(float)xyzWpr.X/1000, (float)xyzWpr.Y/1000, (float)xyzWpr.Z/1000);
-            Vector3 eulerAngles = CreateQuaternionFromFanucWPR((float)xyzWpr.W, (float)xyzWpr.P, (float)xyzWpr.R).eulerAngles;
-            worldPosition.localEulerAngles = new Vector3(eulerAngles.x, -eulerAngles.y, -eulerAngles.z);
+            char[] buffer = new char[1024];
+            while (true)
+            {
+                if (_client.Connected)
+                {
+                    int bytesRead = await _reader.ReadAsync(buffer, 0, buffer.Length);
+                    string data = new string(buffer, 0, bytesRead);
+                    Debug.Log("Received data: " + data); // Add this line to log the received data
 
-            robot[0].localEulerAngles = new Vector3(0, 0, -(float)joint[1]);
-            robot[1].localEulerAngles = new Vector3(0, -(float)joint[2], 0);
-            robot[2].localEulerAngles = new Vector3(0, (float)joint[3] + (float)joint[2], 0);
-            robot[3].localEulerAngles = new Vector3(-(float)joint[4], 0, 0);
-            robot[4].localEulerAngles = new Vector3(0, (float)joint[5], 0);
-            robot[5].localEulerAngles = new Vector3(-(float)joint[6], 0, 0);
+                    string[] values = data.Split(',');
+
+                    // Parse joint angles and xyzwpr position
+                    float[] jointAngles = new float[6];
+                    for (int i = 0; i < 6; i++)
+                    {
+                        jointAngles[i] = float.Parse(values[i]);
+                    }
+
+                    float x = float.Parse(values[6]);
+                    float y = float.Parse(values[7]);
+                    float z = float.Parse(values[8]);
+                    float w = float.Parse(values[9]);
+                    float p = float.Parse(values[10]);
+                    float r = float.Parse(values[11]);
+
+                    UpdateRobotTransforms(jointAngles, new Vector3(x, y, z), new Vector3(w, p, r));
+                }
+            }
         }
 
+
+        private void UpdateRobotTransforms(float[] jointAngles, Vector3 position, Vector3 rotation)
+        {
+            if (robot.Count != jointAngles.Length)
+            {
+                Debug.LogError("Robot joint count doesn't match the joint angles received.");
+                return;
+            }
+
+            robot[0].localEulerAngles = new Vector3(0, 0, -jointAngles[0]);
+            robot[1].localEulerAngles = new Vector3(0, -jointAngles[1], 0);
+            robot[2].localEulerAngles = new Vector3(0, jointAngles[3] + jointAngles[2], 0);
+            robot[3].localEulerAngles = new Vector3(-jointAngles[4], 0, 0);
+            robot[4].localEulerAngles = new Vector3(0, jointAngles[5], 0);
+            robot[5].localEulerAngles = new Vector3(-jointAngles[6], 0, 0);
+
+            worldPosition.localPosition = new Vector3(-position.x / 1000, position.y / 1000, position.z / 1000);
+            Vector3 eulerAngles = CreateQuaternionFromFanucWPR(rotation.x, rotation.y, rotation.z).eulerAngles;
+            worldPosition.localEulerAngles = new Vector3(eulerAngles.x, -eulerAngles.y, -eulerAngles.z);
+        }
+
+        void OnApplicationQuit()
+        {
+            if (_client != null)
+            {
+                _reader.Close();
+                _writer.Close();
+                _client.Close();
+            }
+        }        
+
+        /*
         void RegisterNewPoseIfChanged()
         {
             if (kinect_cursor.localPosition != tempPos || kinect_cursor.localEulerAngles != tempRot)
             {
-                FRCSysPositions sysPositions = _robot.RegPositions;
-                FRCSysPosition sysPosition = sysPositions[1];
-                FRCSysGroupPosition sysGroupPosition = sysPosition.Group[1];
-                FRCXyzWpr xyzWpr = (FRCXyzWpr)sysGroupPosition.Formats[FRETypeCodeConstants.frXyzWpr];
                 xyzWpr.X = -kinect_cursor.localPosition.x * 1000;
                 xyzWpr.Y = kinect_cursor.localPosition.y * 1000;
                 xyzWpr.Z = kinect_cursor.localPosition.z * 1000;
@@ -79,15 +119,10 @@ namespace Telexistence
                 xyzWpr.P = CreateFanucWPRFromQuaternion(kinect_cursor.localRotation).y;
                 xyzWpr.R = CreateFanucWPRFromQuaternion(kinect_cursor.localRotation).z;
                 sysGroupPosition.Update();
-
-                if (sysGroupPosition.IsReachable[Type.Missing, FREMotionTypeConstants.frJointMotionType, FREOrientTypeConstants.frAESWorldOrientType, Type.Missing, out _])
-                {
-
-                }
                 tempPos = gameObject.transform.localPosition;
                 tempRot = gameObject.transform.localEulerAngles;
             }
-        }
+        }*/
 
         private Vector3 CreateFanucWPRFromQuaternion(Quaternion q)
         {
@@ -109,6 +144,6 @@ namespace Telexistence
             float qw = (Mathf.Cos(Rrad / 2) * Mathf.Cos(Prad / 2) * Mathf.Cos(Wrad / 2)) + (Mathf.Sin(Rrad / 2) * Mathf.Sin(Prad / 2) * Mathf.Sin(Wrad / 2));
 
             return new Quaternion(qx, qy, qz, qw);
-        }
+        } 
     }
 }
